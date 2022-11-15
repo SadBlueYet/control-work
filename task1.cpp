21a786e1fd89b11c004f4f04081caeb6ef86321b
SadBlueYet <88283979+SadBlueYet@users.noreply.github.com>
2022-11-15 20:48:01 +0700

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+void userInput();
+int max(int firstNumber, int secondNumber);
+void outputAnswer(int maxNumber);
+int main() {
+    userInput();
+    return 0;
+}
+void userInput() {
+    int firstNumber;
+    int secondNumber;
+    std::cin >> firstNumber >> secondNumber;
+    outputAnswer(max(firstNumber, secondNumber));
+    return;
+}
+int max(int firstNumber, int secondNumber) {
+    if (firstNumber >= secondNumber) return firstNumber;
+    else return secondNumber;
+}
+void outputAnswer(int maxNumber) {
+    std::cout << maxNumber;
+    return;
+}
---
+++ task7.cpp
@@ -0,0 +1,53 @@
+﻿#include <windows.h> 
+#include <iostream>// для Sleep
+#include <cstdio>
+#include <cmath>
+/*void draw_progress_bar(int percents) {
+    int half = percents / 2;
+    std::cout << "\r [";
+    for (int i = 0; i < half; ++i) putc('#', stdout);
+    for (int i = 0; i < 50 - half; ++i) putc(' ', stdout);
+    std::cout << "]" << percents << '%';
+    fflush(stdout);
+}
+
+int main() {
+    printf("\n");
+    for (int i = 0; i <= 100; ++i) {
+        draw_progress_bar(i);
+        Sleep(50);
+    }
+    printf("\n");
+}*/
+void drawCoordinats();
+int main() {
+    drawCoordinats();
+    return 0;
+}
+void drawCoordinats() {
+    char x = (char)219;
+    char y = '|';
+    COORD position = {0, 10};
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hConsole, position);
+    for (int i = 0; i < 20; i++) {
+        std::cout << x;
+        if (i == 10) {
+            COORD position = { 10, 0 };
+            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleCursorPosition(hConsole, position);
+            for (int i = 0; i < 20; i++) {
+                if (i == 9) continue;
+                std::cout << x << '\n' << "          ";
+                
+            }
+        }
+    }
+
+  
+        
+    
+    
+
+}
+
